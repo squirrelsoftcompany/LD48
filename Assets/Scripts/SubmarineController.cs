ef12af19acb92403cf57fbb7309cb1788ad2402e
OroshiX <oroshix@users.noreply.github.com>
2021-04-24 15:36:58 +0200

    [FCT] Submarine Controller (up/down rotation, left/right rotation, forward speed)

--- Assets/Scripts/SubmarineController.cs
+++ Assets/Scripts/SubmarineController.cs
@@ -1,34 +1,59 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[ExecuteInEditMode]
 public class SubmarineController : MonoBehaviour {
     private Rigidbody _rigidbody;
     [SerializeField] private float angularVelocity;
-    [SerializeField] private float speed;
+    [SerializeField] private float velocity;
+    [SerializeField] private float maxVelocity;
+    private float _sqrMaxVelocity;
+
+    private void OnValidate() {
+        setMaxVelocity(maxVelocity);
+    }
+
+    private void setMaxVelocity(float value) {
+        maxVelocity = value;
+        _sqrMaxVelocity = maxVelocity * maxVelocity;
+    }
 
     // Start is called before the first frame update
     private void Start() {
         _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.maxAngularVelocity = Mathf.Deg2Rad * angularVelocity;
+        _sqrMaxVelocity = maxVelocity * maxVelocity;
     }
 
-    // Update is called once per frame
-    private void Update() {
+    private void FixedUpdate() {
+        if (_rigidbody.velocity.sqrMagnitude > _sqrMaxVelocity) {
+            // clamp velocity
+            _rigidbody.velocity = _rigidbody.velocity.normalized * maxVelocity;
+        }
+
         Vector3? wantedDirection = null;
         if (Input.GetKey("up")) {
             // We want to go upward
-            wantedDirection = Quaternion.AngleAxis(-angularVelocity * Time.deltaTime, Vector3.right) *
-                              transform.forward;
+            wantedDirection = Vector3.left;
         } else if (Input.GetKey("down")) {
             // We want to go upward
-            wantedDirection = Quaternion.AngleAxis(angularVelocity * Time.deltaTime, Vector3.up) * transform.forward;
+            wantedDirection = Vector3.right;
+        } else if (Input.GetKey("left")) {
+            // We want to go left
+            wantedDirection = Vector3.down;
+        } else if (Input.GetKey("right")) {
+            wantedDirection = Vector3.up;
         }
 
         print("wanted Dir" + wantedDirection);
-        if (wantedDirection == null) return; // no instruction, do nothing
-        var newRotation = Quaternion.LookRotation((Vector3) wantedDirection);
-        print("newRot" + newRotation);
-        
-        _rigidbody.AddTorque((Vector3) wantedDirection, ForceMode.Impulse);
-        // _rigidbody.MoveRotation(newRotation);
-        _rigidbody.AddForce(transform.forward * (speed ), ForceMode.Force);
+        if (wantedDirection != null) {
+            _rigidbody.AddTorque((Vector3) wantedDirection);
+        }
+
+        if (Input.GetKey("space")) {
+            // Acceleration
+            _rigidbody.AddForce(transform.forward * velocity, ForceMode.Impulse);
+        }
     }
 }
