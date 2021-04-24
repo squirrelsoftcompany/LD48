7714be77e562f007dd4d1f01e7c67974ae4d7760
Xavier Chalut <xavier.chalut@gmail.com>
2021-04-25 01:23:29 +0200

    [FCT] Add post process on main camera and add a main camera prefab

--- Assets/Scripts/PostProcess/PostProcessBlackVeil.cs
+++ Assets/Scripts/PostProcess/PostProcessBlackVeil.cs
@@ -1,20 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 [ExecuteInEditMode]
 public class PostProcessBlackVeil : MonoBehaviour
 {
+    // TODO : Use the "global" depth value instead of an ugly script value
+    public float mDepth = 0;    
+    public PostProcessProfile mPPPBlackVeil;
 
-    public Material mMaterial;
-
-    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    public void Update()
     {
-        Graphics.Blit(source, destination, mMaterial);
-    }
+        Vignette lVignette;
+        ColorGrading lColorGrading;
+        if (mPPPBlackVeil.TryGetSettings(out lVignette))
+        {
+            lVignette.intensity.value = Mathf.Clamp(mDepth / 500.0f, 0.1f, 0.6f);
+        }
 
-    void Update()
-    {
-        //Temporary example of dynamic change of the blackout effect as a function of depth (y axis of the camera) with temporary magic number values
-        float lBlackVeilScale = Mathf.Clamp(1+(transform.position.y / 500.0f), 0.6f, 1.0f);
-        mMaterial.SetFloat("_VRadius", lBlackVeilScale);
+        if (mPPPBlackVeil.TryGetSettings(out lColorGrading))
+        {
+            float lIntensity = Mathf.Clamp(1.0f-(mDepth / 500.0f), 0.5f, 1.0f);
+            lColorGrading.colorFilter.value = new Color(lIntensity, lIntensity, lIntensity);
+        }
     }
 }
