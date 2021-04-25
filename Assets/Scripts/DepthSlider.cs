4e473ff8307db5b4d1fa3e8d8f7a15ce92416c00
OroshiX <oroshix@users.noreply.github.com>
2021-04-25 17:43:22 +0200

    [FIX] Depth slider with correct maximum value

---
+++ Assets/Scripts/DepthSlider.cs
@@ -0,0 +1,14 @@
+using Settings;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DepthSlider : MonoBehaviour {
+    [SerializeField] private Slider slider;
+
+    [SerializeField] private PlayerData playerData;
+
+    // Start is called before the first frame update
+    private void Start() {
+        slider.maxValue = playerData.maxDepth;
+    }
+}
