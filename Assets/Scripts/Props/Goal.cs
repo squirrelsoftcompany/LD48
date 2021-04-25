7a878be57c8fff7f9a5713044bad3b32a00f4bba
OroshiX <oroshix@users.noreply.github.com>
2021-04-25 19:20:04 +0200

    [FCT] Win event (now display a text)

--- Assets/Scripts/Props/Goal.cs
+++ Assets/Scripts/Props/Goal.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 namespace Props {
     public class Goal : MonoBehaviour {
+        [SerializeField] private GameEvent winEvent;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                winEvent.Raise();
+            }
+        }
     }
 }
