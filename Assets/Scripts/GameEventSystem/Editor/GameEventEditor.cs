241d0069791068cfafe412020edf2b07e4d39fad
OroshiX <oroshix@users.noreply.github.com>
2021-04-25 18:20:13 +0200

    [QA] Add editor for game events

---
+++ Assets/Scripts/GameEventSystem/Editor/GameEventEditor.cs
@@ -0,0 +1,19 @@
+using UnityEditor;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+namespace GameEventSystem.Editor {
+    [CustomEditor(typeof(GameEvent))]
+    public class GameEventEditor : UnityEditor.Editor {
+        // Start is called before the first frame update
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+            GUI.enabled = Application.isPlaying;
+            var gameEvent = target as GameEvent;
+            if (GUILayout.Button("Raise")) {
+                Debug.Assert(gameEvent != null, nameof(gameEvent) + " != null");
+                gameEvent.Raise();
+            }
+        }
+    }
+}
