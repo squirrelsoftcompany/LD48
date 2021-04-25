acd80ed2089459235f8023701d0044001413d6a3
OroshiX <oroshix@users.noreply.github.com>
2021-04-25 15:02:49 +0200

    [FCT] Bonus mental health

---
+++ Assets/Scripts/Props/BonusMental.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using Player;
+using UnityEngine;
+
+namespace Props {
+    public class BonusMental : MonoBehaviour {
+        [SerializeField] private float bonusAmount;
+        [SerializeField] private float coolDownSeconds;
+        [SerializeField] private Light light;
+        private float _leftCoolDownTime;
+
+        private void Start() {
+            _leftCoolDownTime = 0;
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            if (_leftCoolDownTime > 0) return;
+
+            var otherHealth = other.GetComponent<MentalHealth>();
+            if (otherHealth == null) return;
+            otherHealth.gainHealth(bonusAmount);
+            sendToCoolDown();
+        }
+
+        private void sendToCoolDown() {
+            StartCoroutine(coolDown());
+        }
+
+        private IEnumerator coolDown() {
+            _leftCoolDownTime = coolDownSeconds;
+            light.intensity = 0.01f;
+            var startTime = Time.time;
+            while (_leftCoolDownTime > 0) {
+                var elapsed = Time.time - startTime;
+                yield return new WaitForFixedUpdate();
+                _leftCoolDownTime = coolDownSeconds - elapsed;
+            }
+            light.intensity = 1f;
+            _leftCoolDownTime = 0;
+        }
+    }
+}
