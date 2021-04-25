8672ecd130c12008a39a01df295c8e2c25a757a6
OroshiX <oroshix@users.noreply.github.com>
2021-04-25 23:20:39 +0200

    [FCT] Init mental health max value

---
+++ Assets/Scripts/MentalSlider.cs
@@ -0,0 +1,14 @@
+using Settings;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MentalSlider : MonoBehaviour {
+    [SerializeField] private Slider slider;
+
+    [SerializeField] private PlayerData playerData;
+
+    // Start is called before the first frame update
+    private void Start() {
+        slider.maxValue = playerData.maxHealth;
+    }
+}
