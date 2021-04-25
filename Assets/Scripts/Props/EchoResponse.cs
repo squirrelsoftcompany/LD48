452694c6201ca8248aef6cca368bbbe27110bbf1
OroshiX <oroshix@users.noreply.github.com>
2021-04-25 18:19:44 +0200

    [FCT] Take into account sound celerity for echolocation response

---
+++ Assets/Scripts/Props/EchoResponse.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace Props {
+    [RequireComponent(typeof(AudioSource))]
+    public class EchoResponse : MonoBehaviour {
+        private AudioSource audioSource;
+        const float CELERITE_SOUND = 343f; // in m/s
+        [SerializeField] private GameEvent echoResponse;
+        private GameObject player;
+
+        // Start is called before the first frame update
+        private void Start() {
+            audioSource = GetComponent<AudioSource>();
+            player = GameObject.FindWithTag("Player");
+        }
+
+        public void respond() {
+            var distance = Vector3.Distance(transform.position, player.transform.position);
+            var time = distance / CELERITE_SOUND;
+            Invoke(nameof(play), time);
+        }
+
+        private void play() {
+            audioSource.Play();
+            echoResponse.Raise();
+        }
+
+        // Update is called once per frame
+        void Update() { }
+    }
+}
