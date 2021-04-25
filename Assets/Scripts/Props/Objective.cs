8ac81316aede4d58a9b7cb0eac00a46e38ce421f
OroshiX <oroshix@users.noreply.github.com>
2021-04-25 02:55:50 +0200

    [WIP] Objective script added

---
+++ Assets/Scripts/Props/Objective.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Props {
+    public class Objective : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start() { }
+
+        public Transform getMyPosition() {
+            return transform;
+        }
+
+        // Update is called once per frame
+        void Update() { }
+    }
+}
