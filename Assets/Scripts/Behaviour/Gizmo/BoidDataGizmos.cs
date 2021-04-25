56fc16a80c98893d07605bcbad4586311f5b8ab4
ChristopheClaustre <christophe.claustre.31@gmail.com>
2021-04-25 11:48:10 +0200

    [FCT] New script to show relevant radius and avoidance radius on gizmos

---
+++ Assets/Scripts/Behaviour/Gizmo/BoidDataGizmos.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Behaviour.Gizmo
+{
+    public class BoidDataGizmos : MonoBehaviour
+    {
+        Behaviour.EnemiesManager.BoidData _boidData;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _boidData = Behaviour.EnemiesManager.Get[tag];
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (!Application.isPlaying)
+                return;
+
+            if (_boidData == null || !_boidData.m_activated)
+                return;
+
+            Gizmos.color = _boidData.m_avoid ? new Color(1, 0, 0, 0.8f) : new Color(0, 1, 0, 0.8f);
+            Gizmos.DrawSphere(transform.position, _boidData.m_avoidanceRadius);
+            Gizmos.DrawWireSphere(transform.position, _boidData.m_relevantRadius);
+        }
+    }
+}
