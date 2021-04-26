d1c6c5a5062b305718bf4712f58f953c1380da2c
Xavier Chalut <xavier.chalut@gmail.com>
2021-04-27 00:25:18 +0200

    [FCT] No need to set the sub gameobject, use the main camera position instead

--- Assets/Scripts/Light/LightIntensity.cs
+++ Assets/Scripts/Light/LightIntensity.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class LightIntensity : MonoBehaviour
 {
-
-    public GameObject mPlayer;
     public float maxIntensity = 100000.0f;
     public float minIntensity = 0.0f;
     public float maxDistance = 1000.0f;
@@ -18,7 +16,7 @@ public class LightIntensity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float dist = Vector3.Distance(mPlayer.transform.position, transform.position - new Vector3(0,100,0));
+        float dist = Vector3.Distance(Camera.main.transform.position, transform.position - new Vector3(0,100,0));
 
         float distanceRange = Mathf.InverseLerp(minDistance, maxDistance, dist);
         float intensity = Mathf.Lerp(Mathf.Sqrt(minIntensity), Mathf.Sqrt(maxIntensity), distanceRange);
