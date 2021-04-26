ea5d6aeda00e05e690ab181b3638e5e1add08367
OroshiX <oroshix@users.noreply.github.com>
2021-04-26 16:38:33 +0200

    [FCT] Destroy fart after duration, and produce a tracker for another duration when using echolocation.

--- Assets/Scripts/Player/Echolocation.cs
+++ Assets/Scripts/Player/Echolocation.cs
@@ -5,22 +5,75 @@ using UnityEngine;
 
 namespace Player {
     public class Echolocation : MonoBehaviour {
-        // We need to drop a fart whenever we use echolocation
-        // This will be used for enemies to find you
-        [SerializeField] private GameObject fartPrefab;
-        [CanBeNull] private GameObject _previousFart;
-        [SerializeField] private GameObject goalMarker;
-        [SerializeField] private float secondsShowGoal;
+        [Tooltip("We need to drop a fart whenever we use echolocation. This will be used for enemies to find you")]
+        [SerializeField]
+        private GameObject fartPrefab;
+
+        [Tooltip(
+            "We need to keep a tracker on whenever we use echolocation. This will be used for enemies to find you")]
+        [SerializeField]
+        private GameObject trackerPrefab;
+
+        [CanBeNull] private GameObject _previousFart, _previousTracker;
+
+        [Tooltip("A prefab to show the direction of the goal")] [SerializeField]
+        private GameObject goalMarker;
+
+        [Tooltip("For how much time we show the goal")] [SerializeField]
+        private float secondsShowGoal;
+
         [CanBeNull] private IEnumerator _showCoroutine;
+
+        [Tooltip("Duration before fart disappears")] [SerializeField]
+        private float fartDuration;
+
+        [Tooltip("Duration before tracker disappears")] [SerializeField]
+        private float trackerDuration;
+
+        [CanBeNull] private IEnumerator _destroyCoroutineFart, _destroyCoroutineTracker;
+
+        private delegate void Method();
 
         public void dropFart() {
             var currentPosition = transform.position;
             // No more than 1 fart
             if (_previousFart != null) {
+                if (_destroyCoroutineFart != null) {
+                    StopCoroutine(_destroyCoroutineFart);
+                }
+
                 Destroy(_previousFart);
             }
 
             _previousFart = Instantiate(fartPrefab, currentPosition, Quaternion.identity);
+            _destroyCoroutineFart = getCoroutineDelay(fartDuration, () => {
+                Destroy(_previousFart);
+                _destroyCoroutineFart = null;
+            });
+            StartCoroutine(_destroyCoroutineFart);
+        }
+
+        public void dropTracker() {
+            // no more than 1 tracker
+            if (_previousTracker != null) {
+                if (_destroyCoroutineTracker != null) {
+                    StopCoroutine(_destroyCoroutineTracker);
+                }
+
+                Destroy(_previousTracker);
+            }
+
+            _previousTracker = Instantiate(trackerPrefab, transform);
+            _destroyCoroutineTracker = getCoroutineDelay(trackerDuration, () => {
+                Destroy(_previousTracker);
+                _destroyCoroutineTracker = null;
+            });
+            StartCoroutine(_destroyCoroutineTracker);
+        }
+
+        private static IEnumerator getCoroutineDelay(float nSeconds, Method method) {
+            yield return new WaitForSeconds(nSeconds);
+            method();
         }
 
         public void showGoalLocation() {
