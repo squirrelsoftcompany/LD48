65f532489dab81a9502388e0e3ccb58e3b07bbff
OroshiX <oroshix@users.noreply.github.com>
2021-04-27 01:16:54 +0200

    [FIX] Light intensity performance

--- Assets/Scripts/Light/LightIntensity.cs
+++ Assets/Scripts/Light/LightIntensity.cs
@@ -1,25 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class LightIntensity : MonoBehaviour
-{
-    public float maxIntensity = 100000.0f;
-    public float minIntensity = 0.0f;
-    public float maxDistance = 1000.0f;
-    public float minDistance = 20.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+namespace Light {
+    public class LightIntensity : MonoBehaviour {
+        public float maxIntensity = 100000.0f;
+        public float minIntensity = 0.0f;
+        public float maxDistance = 1000.0f;
+        public float minDistance = 20.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        float dist = Vector3.Distance(Camera.main.transform.position, transform.position - new Vector3(0,100,0));
+        private UnityEngine.Light myLight;
+        private Camera mainCamera;
 
-        float distanceRange = Mathf.InverseLerp(minDistance, maxDistance, dist);
-        float intensity = Mathf.Lerp(Mathf.Sqrt(minIntensity), Mathf.Sqrt(maxIntensity), distanceRange);
-        GetComponent<Light>().intensity = intensity * intensity;
+        // Start is called before the first frame update
+        private void Start() {
+            myLight = GetComponent<UnityEngine.Light>();
+            mainCamera = Camera.main;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            float dist = Vector3.Distance(mainCamera.transform.position, transform.position - new Vector3(0, 100, 0));
+
+            float distanceRange = Mathf.InverseLerp(minDistance, maxDistance, dist);
+            float intensity = Mathf.Lerp(Mathf.Sqrt(minIntensity), Mathf.Sqrt(maxIntensity), distanceRange);
+            myLight.intensity = intensity * intensity;
+        }
     }
 }
