4b6f681dc975b9db8af615998f18b48d2287ece9
Xavier Chalut <xavier.chalut@gmail.com>
2021-04-26 16:50:44 +0200

    [BUG] Re implement PostProcess using URP

--- Assets/Scripts/PostProcess/PostProcessBlackVeil.cs
+++ Assets/Scripts/PostProcess/PostProcessBlackVeil.cs
@@ -2,25 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using Settings;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 [ExecuteInEditMode]
 public class PostProcessBlackVeil : MonoBehaviour {
     // Use the "global" depth value
     [SerializeField] private GameEvent depthEvent;
     [SerializeField] private PlayerData playerData;
-    public PostProcessProfile mPPPBlackVeil;
+    public VolumeProfile mVPBlackVeil;
 
     public void Update() {
         Vignette lVignette;
-        ColorGrading lColorGrading;
-        if (mPPPBlackVeil.TryGetSettings(out lVignette)) {
+        ColorAdjustments lColorAdjustments;
+        if (mVPBlackVeil.TryGet(out lVignette)) {
             lVignette.intensity.value = Mathf.Clamp(depthEvent.sentFloat / playerData.maxDepth, 0.1f, 0.6f);
         }
 
-        if (mPPPBlackVeil.TryGetSettings(out lColorGrading)) {
+        if (mVPBlackVeil.TryGet(out lColorAdjustments)) {
             float lIntensity = Mathf.Clamp(1.0f - (depthEvent.sentFloat / playerData.maxDepth), 0.5f, 1.0f);
-            lColorGrading.colorFilter.value = new Color(lIntensity, lIntensity, lIntensity);
+            lColorAdjustments.colorFilter.value = new Color(lIntensity, lIntensity, lIntensity);
         }
     }
 }
--- Assets/Scripts/PostProcess/PostProcessMentalHealth.cs
+++ Assets/Scripts/PostProcess/PostProcessMentalHealth.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Settings;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PostProcessMentalHealth : MonoBehaviour
 {
     // Use the "global" depth value
     [SerializeField] private GameEvent mentalHealthEvent;
     [SerializeField] private PlayerData playerData;
-    public PostProcessProfile mPPPMentalHealth;
+    public VolumeProfile mVPMentalHealth;
     private float mCurrentTimerValue = 0.0f;
     private bool mShiftUp= true;
     private float mMaxTimerValue = 2.0f;
@@ -37,22 +38,23 @@ public class PostProcessMentalHealth : MonoBehaviour
             lShift = Mathf.InverseLerp(mMaxTimerValue, 0.0f, mCurrentTimerValue);
         }
 
-        ColorGrading lColorGrading;
+        LiftGammaGain lLiftGammaGain;
         LensDistortion lLensDistortion;
 
-        if (mPPPMentalHealth.TryGetSettings(out lColorGrading))
+        if (mVPMentalHealth.TryGet(out lLiftGammaGain))
         {
             float lMentalHealth = mentalHealthEvent.sentFloat / playerData.maxHealth;
-            float lGammaShift = (lShift* 2.0f * 0.2f - 1.0f) * (1.0f - lMentalHealth);
+            float lGammaShift = ((lShift* 2.0f - 1.0f) * 0.2f) * (1.0f - lMentalHealth);
             // Change intensity between -0.2f and 0.2f 
             lGammaShift = Mathf.Clamp(lGammaShift, -0.2f, 0.2f);
-            lColorGrading.gamma.value = new Vector4(1.0f, 1.0f, 1.0f, lGammaShift);
+            lLiftGammaGain.gamma.value = new Vector4(1.0f, 1.0f, 1.0f, lGammaShift);
         }
-        if (mPPPMentalHealth.TryGetSettings(out lLensDistortion))
+        if (mVPMentalHealth.TryGet(out lLensDistortion))
         {
             float lMentalHealth = mentalHealthEvent.sentFloat / playerData.maxHealth;
             // Change intensity between -60 and +60 
             float lIntensityShift = (lShift* 2.0f * 60.0f - 60.0f) * (1.0f - lMentalHealth);
+            lIntensityShift = lIntensityShift / 100.0f; // URP PP use [-1;1] range
             lIntensityShift = Mathf.Clamp(lIntensityShift, -60.0f, 60.0f);
             lLensDistortion.intensity.value = lIntensityShift;
         }
