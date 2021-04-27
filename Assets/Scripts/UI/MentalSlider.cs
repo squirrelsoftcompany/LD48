3fbf36d6fae91439ee727e2a42fef398f3607112
ChristopheClaustre <christophe.claustre.31@gmail.com>
2021-04-27 02:30:59 +0200

    [FCT] New GUI prefab that will contains all the General UI

--- Assets/Scripts/UI/MentalSlider.cs
+++ Assets/Scripts/UI/MentalSlider.cs
@@ -4,13 +4,26 @@ using UnityEngine.UI;
 
 namespace UI {
     public class MentalSlider : MonoBehaviour {
-        [SerializeField] private Slider slider;
-
         [SerializeField] private PlayerData playerData;
+        private Slider slider;
+        public GameObject mentalPosSpeed;
+        public GameObject mentalNegSpeed1;
+        public GameObject mentalNegSpeed2;
+        public GameObject mentalNegSpeed3;
 
         // Start is called before the first frame update
         private void Start() {
+            slider = GetComponentInChildren<Slider>();
             slider.maxValue = playerData.maxHealth;
+            slider.value = playerData.maxHealth;
+        }
+
+        public void NeoMentalValue(float value)
+        {
+            if (slider)
+            {
+                slider.value = value;
+            }
         }
     }
 }
