42b720d785be882abcd0d25443007fc529faadf5
ChristopheClaustre <christophe.claustre.31@gmail.com>
2021-05-01 20:40:30 +0200

    [FCT] Mental/DepthSlider : Show current direction

--- Assets/Scripts/UI/DepthSlider.cs
+++ Assets/Scripts/UI/DepthSlider.cs
@@ -1,4 +1,6 @@
 using Settings;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,12 +9,41 @@ namespace UI {
         private Slider slider;
 
         [SerializeField] private PlayerData playerData;
+        public GameObject depthPosSpeed;
+        public GameObject depthNegSpeed;
+
+        List<GameObject> depthSpeedSignificants;
 
         // Start is called before the first frame update
         private void Start() {
             slider = GetComponentInChildren<Slider>();
             slider.maxValue = playerData.maxDepth;
             slider.value = playerData.maxDepth;
+            depthSpeedSignificants = new List<GameObject> { depthPosSpeed, depthNegSpeed };
+        }
+
+        public void NeoDepthValue(float value)
+        {
+            if (!slider)
+            {
+                return;
+            }
+
+            StopAllCoroutines();
+            depthSpeedSignificants.ForEach(x => x.SetActive(false));
+
+            float previousValue = slider.value;
+            slider.value = value;
+
+            depthPosSpeed.SetActive(previousValue > value);
+            depthNegSpeed.SetActive(previousValue < value);
+            StartCoroutine(DisactivateSignificantLater());
+        }
+
+        private IEnumerator DisactivateSignificantLater()
+        {
+            yield return new WaitForSeconds(2);
+            depthSpeedSignificants.ForEach(x => x.SetActive(false));
         }
     }
 }
--- Assets/Scripts/UI/MentalSlider.cs
+++ Assets/Scripts/UI/MentalSlider.cs
@@ -1,4 +1,6 @@
 using Settings;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,19 +13,40 @@ namespace UI {
         public GameObject mentalNegSpeed2;
         public GameObject mentalNegSpeed3;
 
+        List<GameObject> mentalSpeedSignificants;
+
         // Start is called before the first frame update
         private void Start() {
             slider = GetComponentInChildren<Slider>();
             slider.maxValue = playerData.maxHealth;
             slider.value = playerData.maxHealth;
+
+            mentalSpeedSignificants = new List<GameObject> { mentalPosSpeed, mentalNegSpeed1, mentalNegSpeed2, mentalNegSpeed3 };
         }
 
         public void NeoMentalValue(float value)
         {
-            if (slider)
+            if (!slider)
             {
-                slider.value = value;
+                return;
             }
+
+            StopAllCoroutines();
+            mentalSpeedSignificants.ForEach(x => x.SetActive(false));
+
+            slider.value = value;
+
+            mentalPosSpeed.SetActive(playerData.ratioSpeedMentalHealth > 0);
+            mentalNegSpeed1.SetActive(playerData.ratioSpeedMentalHealth < 0);
+            mentalNegSpeed2.SetActive(playerData.ratioSpeedMentalHealth < -1/3f);
+            mentalNegSpeed3.SetActive(playerData.ratioSpeedMentalHealth < -2/3f);
+            StartCoroutine(DisactivateSignificantLater());
+        }
+
+        private IEnumerator DisactivateSignificantLater()
+        {
+            yield return new WaitForSeconds(2);
+            mentalSpeedSignificants.ForEach(x => x.SetActive(false));
         }
     }
 }
